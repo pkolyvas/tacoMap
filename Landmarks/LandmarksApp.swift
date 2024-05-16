f5df2d8428fb9494e669562cf281b723a37a21bd
Petros Kolyvas <code@petros.io>
2024-05-16 09:26:56 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Petros Kolyvas on 2024-05-16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
